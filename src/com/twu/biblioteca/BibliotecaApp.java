9b6c07dd54088712a5c9de6a7a2f5399d80cdc70
konglanxin <947350795@qq.com>
2019-04-12 20:02:13 +0800

    getBookList checkoutBook returnBook

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,26 @@
 package com.twu.biblioteca;
+import java.util.*;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Library library = new Library();
+
+        List<String> options = new ArrayList(Arrays.asList(
+                "1-List of books",
+                "2-Checkout a book",
+                "3-Return a book",
+                "4-Quit"));
+        MenuOption menu = new MenuOption(options);
+
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        menu.showMenu();
+        menu.selectOption(library);
+
+
+
+
+
+
     }
 }
